643a2ed42df348e21498a324c43e52a62d7983d9
Rohan Mandal <139258819+rohanmaan07@users.noreply.github.com>
2025-08-16 20:21:22 +0530

    Update AdminRouters.jsx

--- frontend/src/Routers/AdminRouters.jsx
+++ frontend/src/Routers/AdminRouters.jsx
@@ -1,16 +1,35 @@
+// import React from "react";
+// import Admin from "../Admin/Admin";
+// import { Route, Routes, Navigate } from "react-router-dom";  // ✅ Navigate bhi import karo
+// import Navbar from "../components/Navbar";
+// import Footer from "../components/Footer";
+
+// function AdminRouters() {
+//   return (
+//     <div>
+//       <Routes>
+//         {/* ✅ agar sirf /admin hai toh /admin/products pe bhej do */}
+//         <Route path="/" element={<Navigate to="/admin/products" />} />
+
+
+//         {/* ✅ baaki sab Admin component handle karega */}
+//         <Route path="/*" element={<Admin />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default AdminRouters;
 import React from "react";
 import Admin from "../Admin/Admin";
-import { Route, Routes, Navigate } from "react-router-dom";  // ✅ Navigate bhi import karo
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function AdminRouters() {
   return (
     <div>
       <Routes>
         {/* ✅ agar sirf /admin hai toh /admin/products pe bhej do */}
-        <Route path="/" element={<Navigate to="/admin/products" />} />
-
+        <Route path="/" element={<Navigate to="products" />} />
 
         {/* ✅ baaki sab Admin component handle karega */}
         <Route path="/*" element={<Admin />} />
