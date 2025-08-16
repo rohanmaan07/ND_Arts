15921547b0a916c54b57ff63ea2904568e33db5c
Rohan Mandal <139258819+rohanmaan07@users.noreply.github.com>
2025-08-16 18:53:08 +0530

    Update apiConfig.js (backend url)

--- frontend/src/Config/apiConfig.js
+++ frontend/src/Config/apiConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_BASE_URL="http://localhost:8080"
+export const API_BASE_URL="https://nd-arts.onrender.com"
 export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
