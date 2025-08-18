b762f8c9b58c5c31f442a1016fdc53afbf14e5c2
Rohan Mandal <139258819+rohanmaan07@users.noreply.github.com>
2025-08-18 20:44:36 +0530

    Update MainC.js

--- frontend/src/Data/MainC.js
+++ frontend/src/Data/MainC.js
@@ -8,11 +8,11 @@ export const carouselData = [
     path: "/women/clothing/women_dress"
   },
   {
-    image: "https://images.unsplash.com/photo-1520962911456-7db9cfba01b1?auto=format&fit=crop&w=1200&q=80",
+    image: "https://img3.wallspic.com/previews/5/1/9/1/5/151915/151915-harry_potter-house_ties-necktie-suit-bow_tie-x750.jpg",
     path: "/women/clothing/women_dress"
   },
   {
-    image: "https://images.unsplash.com/photo-1618354691865-b30811f7a3e2?auto=format&fit=crop&w=1200&q=80",
+    image: "https://img1.wallspic.com/previews/7/3/9/4/6/164937/164937-tatyana_kozelkina-wicked_ways-model-fai_pea-dress-x750.jpg",
     path: "/women/clothing/women_saree"
   }
 ];
